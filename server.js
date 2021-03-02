9bac78ca0157ec49d656567478e9e0911b73f3cf
Hunter Lacefield <lace1010@gmail.com>
2021-03-02 16:40:01 +0800

    set view engine to pug and redner pug/index in app.route(/)

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const myDB = require("./connection");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
+// Express needs to know which template engine we are using
+app.set("view engine", "pug");
 
 fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
-  res.render('Load your view here');
+app.route("/").get((req, res) => {
+  // I don't know why it is just pug/index. views is not required. If we use "views/pug/index" it will not work
+  res.render("pug/index");
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log("Listening on port " + PORT);
 });

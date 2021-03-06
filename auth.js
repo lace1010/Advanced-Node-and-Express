c0c7bbfb68ce6ae8065d5e1c64128ff5a88071ac
Hunter Lacefield <lace1010@gmail.com>
2021-03-06 13:08:10 +0800

    type

--- auth.js
+++ auth.js
@@ -42,34 +42,11 @@ module.exports = function (app, myDataBase) {
         callbackURL:
           "https://advanced-node-express.herokuapp.com/auth/github/callback",
       },
-      function (accessToken, refreshToken, profile, cb) {
+      (accessToken, refreshToken, profile, cb) => {
         console.log(profile);
-        myDataBase.findOneAndUpdate(
-          { id: profile.id },
-          {
-            $setOnInsert: {
-              id: profile.id,
-              name: profile.displayName || "John Doe",
-              photo: profile.photos[0].value || "",
-              email: Array.isArray(profile.emails)
-                ? profile.emails[0].value
-                : "No public email",
-              created_on: new Date(),
-              provider: profile.provider || "",
-              username: profile.username || "Bish",
-            },
-            $set: {
-              last_login: new Date(),
-            },
-            $inc: {
-              login_count: 1,
-            },
-          },
-          { upsert: true, new: true },
-          (err, doc) => {
-            return cb(null, doc.value);
-          }
-        );
+        myDataBase.findOneAndUpdate({ upsert: true, new: true }, (err, doc) => {
+          return cb(null, doc.value);
+        });
       }
     )
   );
